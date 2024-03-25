42a387833334b7b94877e587973c8fe517763ea1
LucilaAllende <abigail_lucii@hotmail.com>
2024-03-25 18:31:56 -0300

    Add functions to fetch order data, order lines, partner data, and update partner loyalty points

--- odoo-v15/verify_coupon_used.py
+++ odoo-v15/verify_coupon_used.py
@@ -20,7 +20,36 @@ def get_odoo_connection(url, db, username, password):
     print("Error de conexión:", error)
     return None
   
+def fetch_order_data(models, db, uid, password, order_id):
+    """Recupera los datos del pedido desde Odoo."""
+    return models.execute_kw(
+        db, uid, password, 'pos.order', 'search_read',
+        [[('id', '=', order_id), ('state', 'in', ('paid', 'done', 'invoiced'))]],
+        {'order': 'id'}
+    )
 
+def fetch_order_lines(models, db, uid, password, order_lines_ids):
+    """Recupera las líneas de pedido desde Odoo."""
+    return models.execute_kw(
+        db, uid, password, 'pos.order.line', 'search_read',
+        [[('id', 'in', order_lines_ids)]],
+        {'fields': ['product_id', 'qty', 'price_unit']}
+    )
+
+def fetch_partner_data(models, db, uid, password, partner_id):
+    """Recupera los datos del socio desde Odoo."""
+    return models.execute_kw(
+        db, uid, password, 'res.partner', 'search_read',
+        [[('id', '=', partner_id)]]
+    )
+
+def update_partner_loyalty_points(models, db, uid, password, partner_id, new_points):
+    """Actualiza los puntos de lealtad del socio en Odoo."""
+    models.execute_kw(
+        db, uid, password, 'res.partner', 'write',
+        [[partner_id], {'loyalty_points': new_points}]
+    )
+    
 # Establece la conexión con Odoo
 uid = get_odoo_connection(url_taller, db_taller, username_taller, password_taller)
 
@@ -28,41 +57,36 @@ if uid:
   print("Conexión exitosa con Odoo.")
   # Inicializa el punto final de los modelos.
   models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url_taller))
-  # Solicitar al usuario que ingrese id de pedido
-  pos_order_id = input("Por favor, ingresa numero de pedido: ")
-  pos_orders = models.execute_kw(db_taller, uid, password_taller, 'pos.order', 'search_read', [[('id', '=', pos_order_id), ('state', 'in', ('paid', 'done', 'invoiced')) ]], {'order': 'id'})
-  #print(pos_orders) # Habria que validar que el cupon este en estado usado?
+ 
+ # Solicitar al usuario que ingrese el número de pedido
+  pos_order_id = input("Por favor, ingresa el número de pedido: ")
+  pos_orders = fetch_order_data(models, db_taller, uid, password_taller, pos_order_id) # Habria que validar que el cupon este en estado usado?
   if pos_orders:
     pos_order = pos_orders[0]
     amount_total = ((pos_order['amount_total']*12)/100) + pos_order['amount_total']
     print("Monto total del pedido:", amount_total)
+    
     order_lines_ids = pos_order['lines']
-    productos_in_order = models.execute_kw(db_taller, uid, password_taller, 'pos.order.line', 'search_read', 
-                                          [[('id', 'in', order_lines_ids)]], 
-                                          {'fields': ['product_id', 'qty', 'price_unit']})
+    productos_in_order = fetch_order_lines(models, db_taller, uid, password_taller, order_lines_ids)
     print("Productos en el pedido:", productos_in_order)
+    
     # Inicializa una lista vacía para almacenar los IDs de los productos
-    lista_ids_productos = []
-
-    # Itera sobre los resultados y extrae los IDs de los productos
-    for linea_pedido in productos_in_order:
-        # Extrae el ID del producto de la línea del pedido
-        id_producto = linea_pedido['product_id'][0]
-        # Agrega el ID del producto a la lista
-        lista_ids_productos.append(id_producto)
-
+    lista_ids_productos = [linea_pedido['product_id'][0] for linea_pedido in productos_in_order]
     # Imprime la lista de IDs de los productos
     print("Lista de IDs de los productos:", lista_ids_productos)
+    
     program_coupon_id = pos_order['applied_program_ids'][0]
     program_coupon = models.execute_kw(db_taller, uid, password_taller, 'coupon.program', 'search_read', [[('id', '=', program_coupon_id)]], {'fields': ['name']})
     asigment_coupon = models.execute_kw(db_taller, uid, password_taller, 'coupon.program', 'search_read', [[('id', '=', program_coupon_id)]], {'fields': ['assigned_customer']})
     print("Asignacion de cupon:", asigment_coupon)
     partner_id = asigment_coupon[0]['assigned_customer'][0]
-    partner = models.execute_kw(db_taller, uid, password_taller, 'res.partner', 'search_read', [[('id', '=', partner_id)]])
+    partner = fetch_partner_data(models, db_taller, uid, password_taller, partner_id)
     balance_points = partner[0]['loyalty_points']
     print("Puntos acumulados:", balance_points)
+    
     loyalty_program = models.execute_kw(db_taller, uid, password_taller, 'loyalty.program', 'search_read', [[('name', '=', 'AcumulaYGana')]])
     print("Programa de lealtad:", loyalty_program)
+    
     points_won = 0
     rules_id = loyalty_program[0]['rule_ids']
     for rule_id in rules_id:
@@ -74,12 +98,15 @@ if uid:
           # Suma los puntos de la regla al total de puntos ganados
           points_won += int(rule[0]['points_currency'] * amount_total)
     print("Puntos ganados:", points_won)
+    
     # Actualiza el saldo de puntos del cliente
     balance_points += points_won
     # Redondea el balance de puntos a dos decimales
     balance_points = round(balance_points, 2)
-    models.execute_kw(db_taller, uid, password_taller, 'res.partner', 'write', [[partner_id], {'loyalty_points': balance_points}])
+    
+    update_partner_loyalty_points(models, db_taller, uid, password_taller, partner_id, balance_points)
     print("Saldo de puntos actualizado:", balance_points)
-
   else:
     print("No se encontró el pedido.")
+else:
+    print("No se pudo establecer conexión con Odoo.")
