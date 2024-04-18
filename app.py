de7ffa8bed4b2acad4f4e38b3b83ecfef0fdd339
LucilaAllende <abigail_lucii@hotmail.com>
2024-04-18 14:19:42 -0300

    Add Flask SQLAlchemy and migration support

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from modules.process_pos_order_by_client import process_order_pos_bulk
+from models.user import db, User
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///servicat.db'  # Nombre del archivo de la base de datos
+db.init_app(app)
+migrate = Migrate(app, db)
+
 
 # Definimos una ruta para un endpoint
 @app.route('/api/hola', methods=['GET'])
@@ -10,6 +19,7 @@ def hola_mundo():
 @app.route('/webhook/odoo', methods=['POST'])
 def odoo_webhook():
     print('Solicitud recibida desde Odoo:', request)
+    process_order_pos_bulk()
     return jsonify({'mensaje': 'Solicitud recibida desde Odoo'})
 
 if __name__ == '__main__':
