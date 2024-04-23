eaa700f744de1bac52e6986a0eb3df9a8e08d9e7
LucilaAllende <abigail_lucii@hotmail.com>
2024-04-23 14:39:05 -0300

    Add phone field to User model

--- models/user.py
+++ models/user.py
@@ -5,8 +5,10 @@ db = SQLAlchemy()
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(80), unique=False, nullable=False)
+  type_identify = db.Column(db.String(80), unique=False, nullable=False)
   identify = db.Column(db.Integer, unique=True, nullable=False)
   points = db.Column(db.Integer, nullable=False, default=0)
+  phone = db.Column(db.String(80), unique=False, nullable=True)
 
   def __repr__(self):
     return '<User %r>' % self.name
